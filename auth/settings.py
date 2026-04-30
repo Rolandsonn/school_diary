e7033634415c86199a221072d888fa6e9f34ee65
ruslan <stamkulovruslan72@gmail.com>
2026-04-30 17:13:06 +0600

    ok

--- auth/settings.py
+++ auth/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://frontend-school-rzhe.vercel.app/",
+    "https://frontend-school-rzhe.vercel.app",
 ]
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
 AUTH_USER_MODEL = 'my_app.Users'
